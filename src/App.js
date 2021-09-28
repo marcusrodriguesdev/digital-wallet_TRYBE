bae712fba9c3dee81d982fa69567d6532c57fc98
marcus <marcus.rodrigues10@hotmail.com>
2021-09-28 12:01:54 -0300

    finish login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import WalletProvider from './context/WalletProvider';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Login} />
-      </Switch>
+      <WalletProvider>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </WalletProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useContext, useState } from 'react';
 import Input from '../../components/Input';
+import WalletContext from '../../context/WalletContext';
 
-export default function Login() {
+export default function Login({ history }) {
+  const { email, setEmail } = useContext(WalletContext);
+  const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
+
+  function checkPassword() {
+    if (password.length >= 6) {
+      setValidated(true);
+    }
+  }
+
+  function checkEmail() {
+    const regexEmail = /\S+@\S+\.\S+/;
+    const check = regexEmail.test(email);
+    return check;
+  }
+
+  function submitLogin() {
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/carteira');
+  }
+
   return (
     <div>
       Login
@@ -9,10 +32,39 @@ export default function Login() {
         <Input
           type="text"
           name="email"
+          value={email}
           id="email"
+          onChange={({ target }) => {
+            setEmail(target.value);
+            checkPassword();
+          }}
           placeholder="Digite seu email"
         />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          id="password"
+          value={password}
+          onChange={({ target: { value } }) => {
+            setPassword(value);
+            checkPassword();
+          }}
+        />
+        <button
+          type="button"
+          disabled={!validated || !checkEmail()}
+          onClick={() => submitLogin()}
+        >
+          Entrar
+        </button>
       </div>
     </div>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
